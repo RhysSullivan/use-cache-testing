8967b38d13ccf3680e66480e8487da0be55cdadd
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-08-21 11:47:56 -0700

    init

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { Suspense } from "react";
+import { unstable_cacheLife as cacheLife } from "next/cache";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DynamicPage params={params} />
+    </Suspense>
+  );
+}
+
+async function DynamicPage({ params }: { params: Promise<{ id: string }> }) {
+  "use cache";
+  const { id } = await params;
+  // wait 3 seconds
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const renderedAt = new Date().toISOString();
+  cacheLife({ expire: 60 });
+  return (
+    <div>
+      <p>
+        This page has a top level 'use cache' and is accessing no dynamic data.
+        There is no cache tags or cache life on it
+      </p>
+      <p>Rendered at: {renderedAt}</p>
+      <p>ID: {id}</p>
+    </div>
+  );
+}
