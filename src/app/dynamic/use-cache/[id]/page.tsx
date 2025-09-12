9dde7c7b3adcbdadb9cd1d1b41e663021c8c3446
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-09-11 23:50:06 -0700

    more testing

--- src/app/dynamic/use-cache/[id]/page.tsx
+++ src/app/dynamic/use-cache/[id]/page.tsx
@@ -3,15 +3,34 @@ import { checkAuth } from "../../data";
 import { unstable_cacheLife as cacheLife } from "next/cache";
 import { GitHubApi } from "../../github-api";
 async function getData() {
-    'use cache';
-    const githubApi = new GitHubApi();
-    const data = await githubApi.getPullRequestReviewComments("facebook", "react", '34394');
-    return {
-        data: data,
-    };
-}
-
-
+    'use cache'
+    const api = new GitHubApi();
+  const owner = "facebook";
+  const repo = "react";
+  const prNumber = "34394";
+    try {
+      const [prData, diffData, filesData, issueComments, reviewComments] =
+        await Promise.all([
+          api.getPullRequest(owner, repo, prNumber),
+          api.getPullRequestDiff(owner, repo, prNumber),
+          api.getPullRequestFiles(owner, repo, prNumber),
+          api.getPullRequestIssueComments(owner, repo, prNumber),
+          api.getPullRequestReviewComments(owner, repo, prNumber),
+        ]);
+  
+      return {
+        pr: prData,
+        diff: diffData,
+        files: filesData,
+        issueComments,
+        reviewComments,
+      };
+    } catch (error) {
+      console.error("Error fetching PR data:", error);
+      return null;
+    }
+  }
+  
 export async function generateStaticParams() {
     return [];
   }
@@ -26,7 +45,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     const data = await getData();
     return <div>Use Cache Remote
         <p>Auth: {auth ? "true" : "false"}</p>
-        <p>Data: {JSON.stringify(data.data)}</p>
+        <p>Data: {JSON.stringify(data)}</p>
         <p>ID: {id}</p>
     </div>;
 }
