d09d74cf4248ca88e5a394e29c44da1aacd9081b
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-09-12 09:37:16 -0700

    useCache flag

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     cacheComponents: true,
-    clientSegmentCache: true,
+    useCache: true,
   },
   typescript: {
     ignoreBuildErrors: true,
