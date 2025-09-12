ead155d9c1af249e8db37f1dd8ef4779927eee20
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-09-11 23:41:47 -0700

    throw

--- src/app/dynamic/use-cache/[id]/page.tsx
+++ src/app/dynamic/use-cache/[id]/page.tsx
@@ -2,11 +2,20 @@ import { notFound } from "next/navigation";
 import { checkAuth } from "../../data";
 async function getData() {
     'use cache';
+    // wait .1 seconds then throw
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    throw new Error("test");
     await new Promise((resolve) => setTimeout(resolve, 3000));
     return {
         data: new Date().toISOString(),
     };
 }
+
+
+export async function generateStaticParams() {
+    return [];
+  }
+  
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
