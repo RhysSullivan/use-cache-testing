bff96f5be53c51b6416c642b503e1d9b5ee78dc6
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-09-15 17:55:47 -0700

    runtime cache testing

---
+++ src/app/runtime-cache/[id]/debug/route.ts
@@ -0,0 +1,14 @@
+import { getCache } from "@vercel/functions";
+import { NextRequest } from "next/server";
+
+
+export async function GET(request: NextRequest,   { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+    const cache = getCache();
+    if(!id){
+        return new Response('No id provided', { status: 400 });
+    }
+    const data = await cache.get(id);
+    return new Response(JSON.stringify(data), { status: 200 });
+}
---
+++ src/app/runtime-cache/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import { unstable_cacheLife as cacheLife, unstable_cacheTag as cacheTag } from "next/cache";
+
+import { SecondSince } from "../../client";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DynamicPage params={params} />
+    </Suspense>
+  );
+}
+
+async function DynamicPage({ params }: { params: Promise<{ id: string }> }) {
+  "use cache: remote";
+  const { id } = await params;
+  // wait 3 seconds
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const renderedAt = new Date().toISOString();
+  cacheLife({ expire: 60 });
+  cacheTag('runtime-cache-id');
+  return (
+    <div>
+      <p>Rendered at: {renderedAt}</p>
+      <p>ID: {id}</p>
+      <SecondSince start={Date.now()} />
+      <p>Cache expires in 60 seconds</p>
+      <a href='https://github.com/RhysSullivan/use-cache-testing/blob/main/src/app/%5Bid%5D/page.tsx'>View code</a>
+    </div>
+  );
+}
