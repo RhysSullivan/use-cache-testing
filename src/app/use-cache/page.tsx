e2a8205624f35e8323869d0463fb8a85f50b9e90
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-10-14 14:29:15 -0700

    try non dynamic

---
+++ src/app/use-cache/page.tsx
@@ -0,0 +1,32 @@
+import { Suspense } from "react";
+import { unstable_cacheLife as cacheLife, unstable_cacheTag as cacheTag } from "next/cache";
+import { SecondSince } from "../client";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+        <DynamicPage params={params} /> 
+  );
+}
+
+async function DynamicPage({ params }: { params: Promise<{ id: string }> }) {
+  "use cache";
+  const { id } = await params;
+  // wait 3 seconds
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const renderedAt = new Date().toISOString();
+  cacheLife({ expire: 60 });
+  cacheTag(`user-${id}`);
+  return (
+    <div>
+      <p>Rendered at: {renderedAt}</p>
+      <p>ID: {id}</p>
+      <SecondSince start={Date.now()} />
+      <p>Cache expires in 60 seconds</p>
+      <a href='https://github.com/RhysSullivan/use-cache-testing/blob/main/src/app/%5Bid%5D/page.tsx'>View code</a>
+    </div>
+  );
+}
