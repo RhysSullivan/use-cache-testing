466a9e274fafdf1c4c1212691329975a9da5965e
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-10-14 10:11:35 -0700

    expire after 60

---
+++ src/app/use-cache/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { Suspense } from "react";
+import { unstable_cacheLife as cacheLife } from "next/cache";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { SecondSince } from "@/app/client";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DynamicPage params={params} />
+    </Suspense>
+  );
+}
+
+async function getUser(id: string) {
+    'use cache'
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    cacheTag(`user-${id}`);
+    cacheLife({ expire: 60 });
+  return {
+    id,
+    name: "John Doe",
+  };
+}
+
+async function DynamicPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const user = await getUser(id);
+  const renderedAt = new Date().toISOString();
+  return (
+    <div>
+      <p>Rendered at: {renderedAt}</p>
+      <p>ID: {id}</p>
+      <SecondSince start={Date.now()} />
+      <p>Cache expires in 60 seconds</p>
+      <a href='https://github.com/RhysSullivan/use-cache-testing/blob/main/src/app/%5Bid%5D/page.tsx'>View code</a>
+    </div>
+  );
+}
